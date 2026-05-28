4d0c587ec5634af218cf2ce2674578df2f218d29
ldrichmondg <luis.richmondg@gmail.com>
2026-05-28 15:54:57 -0600

    feat(conocimiento): se crean todos los cursos

---
+++ conocimiento/cursos.pl
@@ -0,0 +1,73 @@
+% ===== SEMESTRE 0 =====
+curso(examen_diagnostico, 'Examen Diagnóstico').
+curso(ingles_basico, 'Inglés Básico').
+curso(matematica_general, 'Matemática General').
+
+% ===== SEMESTRE 1 =====
+curso(comunicacion_escrita, 'Comunicación Escrita').
+curso(fundamentos_organizacion_computadoras, 'Fundamentos de Organización de Computadoras').
+curso(introduccion_programacion, 'Introducción a la Programación').
+curso(taller_programacion, 'Taller de Programación').
+curso(matematica_discreta, 'Matemática Discreta').
+curso(actividad_cultural_1, 'Actividad Cultural I').
+
+% ===== SEMESTRE 2 =====
+curso(comunicacion_oral, 'Comunicación Oral').
+curso(ingles_1, 'Inglés I').
+curso(centros_formacion_humanistica, 'Centros de Formación Humanística').
+curso(estructuras_datos, 'Estructuras de Datos').
+curso(programacion_orientada_objetos, 'Programación Orientada a Objetos').
+curso(arquitectura_computadores, 'Arquitectura de Computadores').
+curso(calculo_diferencial_integral, 'Cálculo Diferencial e Integral').
+curso(actividad_deportiva_1, 'Actividad Deportiva I').
+
+% ===== SEMESTRE 3 =====
+curso(ingles_2, 'Inglés II').
+curso(analisis_algoritmos, 'Análisis de Algoritmos').
+curso(bases_datos_1, 'Bases de Datos I').
+curso(requerimientos_software, 'Requerimientos de Software').
+curso(calculo_algebra_lineal, 'Cálculo y Álgebra Lineal').
+curso(actividad_cultural_deportiva, 'Actividad Cultural-Deportiva').
+
+% ===== SEMESTRE 4 =====
+curso(ambiente_humano, 'Ambiente Humano').
+curso(bases_datos_2, 'Bases de Datos II').
+curso(lenguajes_programacion, 'Lenguajes de Programación').
+curso(diseno_software, 'Diseño de Software').
+curso(probabilidades, 'Probabilidades').
+
+% ===== SEMESTRE 5 =====
+curso(seminario_estudios_filosoficos_historicos, 'Seminario de Estudios Filosóficos Históricos').
+curso(administracion_proyectos, 'Administración de Proyectos').
+curso(compiladores_interpretes, 'Compiladores e Intérpretes').
+curso(aseguramiento_calidad, 'Aseguramiento de la Calidad del Software').
+curso(estadistica, 'Estadística').
+
+% ===== SEMESTRE 6 =====
+curso(seminario_estudios_costarricenses, 'Seminario de Estudios Costarricenses').
+curso(electiva_1, 'Electiva I').
+curso(investigacion_operaciones, 'Investigación de Operaciones').
+curso(principios_sistemas_operativos, 'Principios de Sistemas Operativos').
+curso(computacion_sociedad, 'Computación y Sociedad').
+curso(seguridad_software, 'Seguridad del Software').
+
+% ===== SEMESTRE 7 =====
+curso(desarrollo_emprendedores, 'Desarrollo de Emprendedores').
+curso(electiva_2, 'Electiva II').
+curso(inteligencia_artificial, 'Inteligencia Artificial').
+curso(redes, 'Redes').
+curso(proyecto_ingenieria_software, 'Proyecto de Ingeniería de Software').
+
+% ===== SEMESTRE 8 =====
+curso(practica_profesional, 'Práctica Profesional').
+
+
+% ===== ELECTIVAS =====
+curso(criptografia, 'Criptografía').
+curso(almacenes_datos_procesamiento_olap, 'Almacenes de Datos y Procesamiento OLAP').
+curso(introduccion_analisis_datos, 'Introducción al Análisis de Datos').
+curso(desarrollo_web, 'Desarrollo Web').
+curso(desarrollo_videojuegos, 'Desarrollo de Videojuegos').
+curso(sistemas_informacion_geografica, 'Sistemas de Información Geográfica').
+curso(visualizacion_informacion, 'Visualización de Información').
+curso(introduccion_reconocimiento_patrones, 'Introducción al Reconocimiento de Patrones').
